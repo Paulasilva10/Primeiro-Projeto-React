27a1d3cbd1691413098b322236010ca340d042a5
Paulasilva10 <paulasilva0ad@gmail.com>
2024-01-11 08:18:26 -0300

    2 code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { v4 as uuid } from 'uuid';
 import React, { useState } from 'react';
-import { Container, ToDoList, Input, Button, ListItem,  } from './styles.js'
-import { FcFullTrash, FcCheckmark } from "react-icons/fc";
+import { Container, ToDoList, Input, Button, ListItem, Trash, Check } from './styles.js'
+
 
 //sintaxe JSX
 function App() {
   //codigo js
   //retorna codigo html
 
-  const [list, setList] = useState([{ id: uuid(), task: "Adicionar uma tarefa", finished: true }]);
+  const [list, setList] = useState([]);
   const [inputTask, setInputTask] = useState('');
   //"Levar o Nico para passear","Terminar as aulas de React no DevClub"
 
@@ -18,30 +18,45 @@ function App() {
   }
 
   function cliqueinobotao() {
+    if(inputTask){
     setList([...list, { id: uuid(), task: inputTask, finished: false }]);
-
+}
   }
 
-  function finalizarTarefa(id: any): React.MouseEventHandler<SVGElement> | undefined {
-    throw new Error('Function not implemented.');
+  function finalizarTarefa(id) {
+
+    const newList = list.map(item => (
+      item.id === id ? { ...item, finished: !item.finished } : item
+    ))
+    setList(newList)
   }
 
+  function deletarItem(id) {
+    const newList = list.filter(item => item.id !== id)
+    setList(newList)
+  }
 
   return (
     <Container>
+      
       <ToDoList>
+      <h2>Lista de Tarefas</h2>
         <Input onChange={inputMudou} placeholder="O que tenho para fazer" type="text" />
         <Button onClick={cliqueinobotao} >Adicionar</Button>
-
+        
         <ul>
           {
-            list.map((item) => (
-              <ListItem isFinished={item.finished} key={item.id} >
-                <FcCheckmark />
-                <li >{item.task}</li>
-                <FcFullTrash />
-              </ListItem>
-            ))}
+            list.length > 0 ? (
+              list.map((item) => (
+                <ListItem isFinished={item.finished} key={item.id}>
+                  <Check onClick={() => finalizarTarefa(item.id)} />
+                  <li >{item.task}</li>
+                  <Trash onClick={() => deletarItem(item.id)} />
+                </ListItem>
+              ))
+            ) : (
+              <h3>Não há itens na lista</h3>
+            )}
         </ul>
       </ToDoList>
     </Container>
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import { FcFullTrash, FcCheckmark } from "react-icons/fc";
+
 export const Container = styled.div`
 background: linear-gradient(90deg, #383838 0%, #000000 81.25%);
 width: 100vw;
@@ -18,6 +20,23 @@ ul {
     padding: 0;
     margin-top: 60 px;
 }
+
+h3 {
+    display: flex;
+justify-content: center;
+align-items: center;
+color: #A8A8A8 ;
+font-size: 14px; 
+font-weight: lighter;
+}
+
+h2 {
+display: flex;
+justify-content: center;
+align-items: center;
+color: blueviolet ;
+font-weight: 500;
+}
 `
 
 export const Input = styled.input`
@@ -35,13 +54,19 @@ width: 130px;
 height: 40px;
 border-radius: 5px;
 border: none;
-
-
-
+cursor: pointer;
 font-size: 17px;
 font-weight: 900;
 line-height: 3px;
 color: white;
+&:hover {
+    opacity: 0.8;
+}
+&:active{
+    opacity: 0.6;
+}
+
+
 `
 
 export const ListItem = styled.div`
@@ -63,4 +88,10 @@ li{
 
 `
 
+export const Trash = styled(FcFullTrash) `
+cursor: pointer;
+`
 
+export const Check = styled(FcCheckmark) `
+cursor: pointer;
+`
